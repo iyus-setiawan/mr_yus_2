65e63ad1909929a3b09249a9f996bc3fd6bc4b8b
iyus-setiawan <iyuss82@gmail.com>
2024-08-14 20:51:43 +0700

    modif index.php

--- Index.php
+++ Index.php
@@ -9,5 +9,6 @@
 <body>
   <h1>hello word</h1>
   <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cupiditate ipsum maxime quo sed vero perferendis deserunt, aliquid molestias excepturi quibusdam ad blanditiis eius magnam quidem quasi non modi. Laudantium.</p1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis dolor dolore minima, aliquid eveniet sunt minus provident, ullam error, obcaecati libero aliquam sint nemo delectus nesciunt quas, eum deserunt accusantium.</p>
 </body>
 </html>
