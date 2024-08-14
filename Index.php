fc37d301e19624db06e5e68ab3dc9ad3875b2f83
iyus-setiawan <iyuss82@gmail.com>
2024-08-14 20:00:17 +0700

    merubah index.php

--- Index.php
+++ Index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
   <h1>hello word</h1>
+  <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cupiditate ipsum maxime quo sed vero perferendis deserunt, aliquid molestias excepturi quibusdam ad blanditiis eius magnam quidem quasi non modi. Laudantium.</p1>
 </body>
 </html>
